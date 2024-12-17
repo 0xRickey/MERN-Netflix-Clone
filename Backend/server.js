fdec20df203e9ad1b9f1035916f3886d2471a0ae
Rickey <95059419+0xRickey@users.noreply.github.com>
2024-12-17 16:03:07 +1100

    Updated server.js to use the authentication routes in auth.routes.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,10 @@
 import express from "express"
 
+import authRoutes from "./routes/auth.routes.js"
+
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Server is Ready!")
-})
+app.use("/api/v1/auth/", authRoutes)
 
 app.listen(4269, () => {
     console.log("Server started at http://localhost:4269")
