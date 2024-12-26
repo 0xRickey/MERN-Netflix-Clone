3642a151fe9c5bb83e32c12faa052a73913692e5
Rickey <95059419+0xRickey@users.noreply.github.com>
2024-12-26 19:20:55 +1100

    Added code to server.js to run the code to connect to the MongoDB cluster

--- Backend/server.js
+++ Backend/server.js
@@ -1,12 +1,16 @@
 import express from "express"
 
 import authRoutes from "./routes/auth.routes.js"
+import { ENV_VARS } from "./config/env-vars.js";
+import { connectDB } from "./config/db.js";
 
 const app = express();
+const PORT = ENV_VARS.PORT
 
 app.use("/api/v1/auth/", authRoutes)
 
-app.listen(4269, () => {
-    console.log("Server started at http://localhost:4269")
+app.listen(PORT, () => {
+    console.log(`Server started at http://localhost:${PORT}`)
+    connectDB();
 })
 
